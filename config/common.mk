b1e10a2d6c0d578191ff1b65ee98593f567e1fa8
David <daviddpruitt@gmail.com>
2013-06-19 14:52:14 -0600

    changed name from UNOFFICIAL to UTEPRobust
    Change-Id: I74840858d7b18b845fa92815c97c71d410523420

--- config/common.mk
+++ config/common.mk
@@ -203,7 +203,7 @@ ifdef CM_BUILDTYPE
     endif
 else
     # If CM_BUILDTYPE is not defined, set to UNOFFICIAL
-    CM_BUILDTYPE := UNOFFICIAL
+    CM_BUILDTYPE := UTEPRobust
     CM_EXTRAVERSION :=
 endif
 
